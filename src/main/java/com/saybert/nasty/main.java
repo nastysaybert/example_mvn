58c43c22bfa0cc6cd71be64a41947d3bfc6f8018
nastysaybert <nasty@saybert.ru>
2020-10-13 01:25:25 +0300

    Попробовала Maven

---
+++ src/main/java/com/saybert/nasty/main.java
@@ -0,0 +1,12 @@
+package com.saybert.nasty;
+
+/**
+ * @See https://cwiki.apache.org/confluence/display/MAVEN/Maven+Properties+Guide
+ * @See https://stackoverflow.com/questions/4757426/maven-specify-the-outputdirectory-only-for-packaging-a-jar
+ */
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Maven!");
+    }
+}
+
